aad9d6dcf3b03bfee6e6d4a6d07eb16d815cbd6e
ITD <itd@ITDs-iMac.local>
2023-07-07 17:44:46 +0800

    re-import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
-import Navbar from "./components/navBar";
 import { BannerSeparator } from "./components/BannerSeparator";
 import { BestSection } from "./components/BestSection";
 import { FeaturedSection } from "./components/FeaturedSection";
 import { CTASection } from "./components/CTASection";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
